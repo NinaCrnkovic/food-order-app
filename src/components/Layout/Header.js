973ff5516fdbb8e2fd35e93d2df8317e29f214d0
NinaCrnkovic <nina.crnkovic.zg@gmail.com>
2022-10-27 13:14:50 +0200

    changed header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,8 +6,10 @@ import classes from "./Header.module.css"
 const Header =(props)=>{
 return(
     <React.Fragment>
-        <header className={classes.header}>ReactMeals</header>
+        <header className={classes.header}>
+           <h1>ReactMeals</h1> 
         <button>Cart</button>
+        </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of food!"/>
         </div>
